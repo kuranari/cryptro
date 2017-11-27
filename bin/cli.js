d00baa13c3ea275d5334224c1d9e4cd36d97feea
Tomohisa Kuranari <m.qureana@gmail.com>
2017-11-27 18:07:33 +0900

    initial commit

---
+++ bin/cli.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+'use strict';
+
+const minimist = require("minimist");
+const fs = require('fs-extra');
+const crypto = require('crypto');
+const { edit, read, write, fetchKey } = require('../lib/enset');
+
+const encFilepath = 'secrets.yml.enc';
+const keyFilePath = '.secrets.key'
+
+const argv = minimist(process.argv.slice(2));
+const subcommand = argv._[0];
+const file = argv._[1] || encFilepath;
+
+function usage() {
+  console.log('usage: enset <command>');
+  console.log('where <command> is on of:');
+  console.log('  edit, read, write, setup');
+}
+
+
+switch(subcommand) {
+  case 'edit': {
+    const key = fetchKey({ path: keyFilePath });
+    edit(file, key);
+    break;
+  }
+  case 'read': {
+    const key = fetchKey({ path: keyFilePath });
+    console.log(read(file, key));
+    break;
+  }
+  case 'write': {
+    const key = fetchKey({ path: keyFilePath });
+    const data = fs.readFileSync('/dev/stdin', 'utf8').replace(/\n$/, '');
+
+    write(file, data, key);
+    break;
+  }
+  case 'setup': {
+    const randomValue = crypto.randomBytes(16).toString('hex');
+    try {
+      fs.writeFileSync(keyFilePath, randomValue, { flag: 'wx' });
+      console.log(`created: ${keyFilePath}`);
+    } catch (err) {
+      if (err.code !== 'EEXIST') { throw err }
+      console.log(`file already exists: ${err.path}`);
+    }
+
+    try {
+      fs.writeFileSync(file, "", { flag: 'wx' })
+
+      const key = fetchKey({ path: keyFilePath });
+      write(file, 'awsomeValue: 42', key);
+      console.log(`created: ${file}`);
+    } catch (err) {
+      if (err.code !== 'EEXIST') { throw err }
+      console.log(`file already exists: ${err.path}`);
+    }
+    break;
+  }
+  default:
+    if (subcommand) console.log(`Unexpected command: ${subcommand}\n`);
+    usage();
+}
--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-https://qiita.com/ysti/items/05aaf14b303ff6b13519
-var crypto = require('crypto');
-var algorithm = 'aes-256-ctr';
-var passphrase = "7IeZlmfz";
+const Enset = require('./lib/enset');
 
-var encrypt = (text) => {
-  var cipher = crypto.createCipher(algorithm,passphrase)
-  var crypted = cipher.update(text,'utf8','base64')
-  crypted += cipher.final('base64');
-  return crypted;
-}
+const key = Enset.fetchKey({ path: '.secrets.key' })
+const settings = Enset.load('secrets.yml.enc', key);
 
-var decrypt = (text) => {
-  var decipher = crypto.createDecipher(algorithm,passphrase)
-  var dec = decipher.update(text,'base64','utf8')
-  dec += decipher.final('utf8');
-
-  return dec;
-}
+console.log(settings);
